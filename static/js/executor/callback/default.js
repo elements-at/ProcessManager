1475d3996d56b191261bff438778ed0971b2d945
ckogler <t23rKIgoelxSI22>
2016-07-06 10:40:36 +0200

    Remove files because of restructoring

--- static/js/executor/callback/default.js
+++
@@ -1,6 +0,0 @@
-pimcore.registerNS("pimcore.plugin.processmanager.executor.callback.default");
-pimcore.plugin.processmanager.executor.callback.default = Class.create(pimcore.plugin.processmanager.executor.callback.abstractCallback,{
-
-
-    
-});
