6c34b5c5c8900727dcb716109b61e621571a2aed
ckogler <t23rKIgoelxSI22>
2016-07-15 12:14:03 +0200

    Cleaned Files

--- static/js/executor/callback/default.js
+++
@@ -1,7 +0,0 @@
-pimcore.registerNS("pimcore.plugin.processmanager.executor.callback.default");
-pimcore.plugin.processmanager.executor.callback.default = Class.create(pimcore.plugin.processmanager.executor.callback.abstractCallback,{
-    name : "default"
-
-
-    
-});
