a6e4ee6c78f60aa567a0d9ad02b4c4ba5f12b06c
ckogler <christian.kogler@elements.at>
2016-12-09 15:12:02 +0100

    Added ScheduleManager to execute multiple jobs...

---
+++ models/ProcessManager/Schedule/Manager/Procedural.php
@@ -0,0 +1,36 @@
+<?php
+namespace ProcessManager\Schedule\Manager;
+
+
+class Procedural extends \Pimcore\Model\Schedule\Manager\Procedural {
+
+
+    public function run()
+    {
+        $monitoringItem = \ProcessManager\Plugin::getMonitoringItem();
+        $logger = $monitoringItem->getLogger();
+
+        $this->setLastExecution();
+
+        $totalSteps = count($this->jobs);
+        $monitoringItem->setTotalSteps($totalSteps)->save();
+        $i = 0;
+        foreach ($this->jobs as $job) {
+            $i++;
+            $job->lock();
+
+            $monitoringItem->setCurrentStep($i)->setMessage("Executing job with ID: " . $job->getId())->resetWorkload()->save();
+            try {
+                $job->execute();
+                $monitoringItem->setMessage("Finished job with ID: " . $job->getId())->save();
+            } catch (\Exception $e) {
+                $monitoringItem->setMessage("Failed to execute job: " . $job->getId())->save();
+                $logger->emergency("Failed to execute job with id: " . $job->getId());
+                $logger->error($e);
+            }
+            $job->unlock();
+            $monitoringItem->setWorloadCompleted()->save();
+        }
+    }
+
+}
