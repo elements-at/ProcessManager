0d16d9fcab8be4c2fd332c4bc2cbaf03553fef54
ckogler <christian.kogler@elements.at>
2020-01-13 11:45:25 +0100

    MonitoringItem::getCallbackSettings() : json_decode fails as settings can be empty array
    https://github.com/elements-at/ProcessManager/issues/44

--- src/Model/MonitoringItem.php
+++ src/Model/MonitoringItem.php
@@ -700,7 +700,7 @@ class MonitoringItem extends \Pimcore\Model\AbstractModel
      */
     public function getCallbackSettings()
     {
-        return json_decode($this->callbackSettings, true);
+        return $this->callbackSettings;
     }
 
     public function getCallbackSettingsForGrid()
