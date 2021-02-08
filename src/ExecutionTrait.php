cc7991b93e3c75c0ee653c64b5c7b17790d862ab
ckogler-elements <christian.kogler@elements.at>
2021-02-08 10:27:30 +0100

    Check executing user only if posix_getpwuid is available (seems to be not available in nginx)

--- src/ExecutionTrait.php
+++ src/ExecutionTrait.php
@@ -227,18 +227,22 @@ trait ExecutionTrait
     {
         $configFile = \Pimcore\Config::locateConfigFile('system.yml');
         $owner = fileowner($configFile);
+
         if ($owner === false) {
             throw new \Exception("Couldn't get user from file " . $configFile);
         }
-        $userData = posix_getpwuid($owner);
-        $allowedUsers[] = $userData['name'];
+        if(function_exists('posix_getpwuid')){
+            $userData = posix_getpwuid($owner);
+            $allowedUsers[] = $userData['name'];
 
-        $scriptExecutingUserData = posix_getpwuid(posix_geteuid());
-        $scriptExecutingUser = $scriptExecutingUserData['name'];
+            $scriptExecutingUserData = posix_getpwuid(posix_geteuid());
+            $scriptExecutingUser = $scriptExecutingUserData['name'];
 
-        if (!in_array($scriptExecutingUser, $allowedUsers)) {
-            throw new \Exception("The current system user is not allowed to execute this script. Allowed users: '" . implode(',', $allowedUsers) ."' Executing user: '$scriptExecutingUser'.");
+            if (!in_array($scriptExecutingUser, $allowedUsers)) {
+                throw new \Exception("The current system user is not allowed to execute this script. Allowed users: '" . implode(',', $allowedUsers) ."' Executing user: '$scriptExecutingUser'.");
+            }
         }
+
     }
 
     /**
