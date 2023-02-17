ac27572314672d02eacb6e808479814f54ee988e
ckogler <christian.kogler@elements.at>
2023-02-17 11:35:43 +0100

    fix: Restrict Process execution to Permission #166

---
+++ src/Migrations/Version20230217000000.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Elements\Bundle\ProcessManagerBundle\Migrations;
+
+use Elements\Bundle\ProcessManagerBundle\ElementsProcessManagerBundle;
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\BundleAwareMigration;
+
+class Version20230217000000 extends BundleAwareMigration
+{
+    protected function getBundleName(): string
+    {
+        return ElementsProcessManagerBundle::BUNDLE_NAME;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+
+        $this->addSql(
+            'UPDATE bundle_process_manager_configuration SET `restrictToPermissions` = "" WHERE `restrictToPermissions` IS null'
+        );
+        $this->addSql(
+            'ALTER TABLE `bundle_process_manager_configuration` MODIFY `restrictToPermissions`  MEDIUMTEXT NOT NULL DEFAULT ""'
+        );
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+    }
+}
