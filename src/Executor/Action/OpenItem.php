d673a0f1e9fd3a70a90c3c49eee0f48fc074a307
APochmann <96241555+APochmann@users.noreply.github.com>
2023-03-09 12:18:46 +0100

    Modify check for finished state
    Use new function to check finished state dependent on 'allowErrorsOnFinish' configuration flag

--- src/Executor/Action/OpenItem.php
+++ src/Executor/Action/OpenItem.php
@@ -120,7 +120,7 @@ class OpenItem extends AbstractAction
      */
     public function getGridActionHtml($monitoringItem, $actionData)
     {
-        if ($monitoringItem->getStatus() == $monitoringItem::STATUS_FINISHED) {
+        if ($monitoringItem->isFinished()) {
 
             $item = $this->getItem($monitoringItem,$actionData);
             if ($item) {
@@ -144,7 +144,7 @@ class OpenItem extends AbstractAction
         $data['item_exists'] = false;
         $data['item_type'] = null;
 
-        if ($monitoringItem->getStatus() == $monitoringItem::STATUS_FINISHED) {
+        if ($monitoringItem->isFinished()) {
             $item = $this->getItem($monitoringItem,$actionData);
             if($item) {
                 $data['item_exists'] = true;
